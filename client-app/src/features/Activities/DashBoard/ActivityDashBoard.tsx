632ece16bb19316b4906f2a207edada2f2a7e292
Neo Masemola <nggmasemola@outlook.com>
2020-04-20 01:29:20 +0200

    Adding CRUD Functionality to the cleint-app for activities

---
+++ client-app/src/features/Activities/DashBoard/ActivityDashBoard.tsx
@@ -0,0 +1,41 @@
+import React, { SyntheticEvent } from 'react';
+import {Grid} from 'semantic-ui-react'
+import { IActivity } from "../../../app/models/activity";
+import {ActivityList} from '../DashBoard/ActivityList';
+import { ActivityDetails } from '../Details/ActivityDetails';
+import { ActivityForm } from '../form/ActivityForm';
+
+interface IProps {
+    activities: IActivity[];
+    selectActivity: (id: string) => void;
+    selectedActivity: IActivity | null;
+    editMode : boolean;
+    setEditMode: (editMode: boolean) => void;
+    setSelectedActivity : (selectActivity : IActivity | null) => void;
+    createActivity: (activity: IActivity) => void;
+    editActivity: (activity: IActivity) => void;
+    deleteActivity: (Event: SyntheticEvent<HTMLButtonElement>, id: string) => void;
+    submitting: boolean;
+    target: string
+}
+
+export const ActivityDashBoard:React.FC<IProps> = ({activities, selectActivity, selectedActivity, editMode, setEditMode, setSelectedActivity, createActivity, editActivity, deleteActivity, submitting, target}) => {
+    return (
+       <Grid>
+           <Grid.Column width={10}>
+                <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} submitting={submitting} target={target}/>
+           </Grid.Column>
+           <Grid.Column width={6}>
+                {selectedActivity && !editMode  && (<ActivityDetails activity={selectedActivity} setEditMode={setEditMode} setSelectedActivity={setSelectedActivity}/>)}
+                {editMode && 
+                    <ActivityForm 
+                        key={selectedActivity && selectedActivity.id || 0}
+                        setEditMode={setEditMode} 
+                        activity={selectedActivity!} 
+                        createActivity={createActivity} 
+                        editActivity={editActivity} 
+                        submitting={submitting}/>}
+           </Grid.Column>
+       </Grid>
+    )
+}
